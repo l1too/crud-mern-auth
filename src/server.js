1692060ab39585f7683c9bfde70ae920005c2e3a
Gabriel Quiroga <gabrielnahuel.quiroga@gmail.com>
2024-01-05 04:57:47 -0300

    agregando CRUD de tareas

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import authRoutes from './routes/auth.routes.js'
 import cookieParser from 'cookie-parser'
+import tasksRoutes from './routes/tasks.routes.js'
 
 const app = express()
 
@@ -10,5 +11,6 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api",authRoutes)
+app.use("/api",tasksRoutes)
 
 export default app
