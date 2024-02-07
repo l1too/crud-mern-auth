2331ce4282c7d802e9f4b9b0904d7d76427be7ef
Gabriel Quiroga <gabrielnahuel.quiroga@gmail.com>
2024-02-06 22:11:01 -0300

    agregando update task

--- client/src/context/TaskContext.jsx
+++ client/src/context/TaskContext.jsx
@@ -1,49 +1,76 @@
 import { createContext, useContext, useState } from "react";
-import { createTaskRequest, deleteTaskRequest, getTasksRequest } from "../api/tasks";
+import {
+  createTaskRequest,
+  deleteTaskRequest,
+  getTasksRequest,
+  getTaskRequest,
+  updateTaskRequest,
+} from "../api/tasks";
 
-const TaskContext = createContext()
+const TaskContext = createContext();
 
+export const useTasks = () => {
+  const context = useContext(TaskContext);
+  if (!context) throw new Error("useTasks must be used within a TaskProvider");
+  return context;
+};
 
-export const useTasks = ()=> {
-    const context = useContext(TaskContext)
-    if(!context) {
-        throw new Error("useTasks must be used within a TaskProvider")
+// eslint-disable-next-line react/prop-types
+export function TaskProvider({ children }) {
+  const [tasks, setTasks] = useState([]);
+
+  const getTasks = async () => {
+    const res = await getTasksRequest();
+    setTasks(res.data);
+  };
+
+  const deleteTask = async (id) => {
+    try {
+      const res = await deleteTaskRequest(id);
+      if (res.status === 204) setTasks(tasks.filter((task) => task._id !== id));
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return context
-}
-
-export function TaskProvider({children}){
-
-    const [tasks, setTasks] = useState([])
-
-    const getTasks = async ()=> {
-        try {
-            const res = await getTasksRequest()
-            setTasks(res.data)
-        } catch (error) {
-            console.error(error)
-        }
+  const createTask = async (task) => {
+    try {
+      const res = await createTaskRequest(task);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const createTask = async (task)=> {
-            const res = await createTaskRequest(task)
-            console.log(res);
+  const getTask = async (id) => {
+    try {
+      const res = await getTaskRequest(id);
+      return res.data;
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    const deleteTask = async (id)=> {
-        try {
-            const res = await deleteTaskRequest(id)
-            console.log(res);
-            if(res.status === 202) setTasks(tasks.filter(task => task._id !== id))
-        } catch (error) {
-            console.log(error);
-        }
+  const updateTask = async (id, task) => {
+    try {
+      await updateTaskRequest(id, task);
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    return(
-        <TaskContext.Provider value={{tasks, createTask, getTasks, deleteTask}}>
-            {children}
-        </TaskContext.Provider>
-    )
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        getTasks,
+        deleteTask,
+        createTask,
+        getTask,
+        updateTask,
+      }}
+    >
+      {children}
+    </TaskContext.Provider>
+  );
 }
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,23 +1,41 @@
 import { useForm } from "react-hook-form"
 import { useTasks } from "../context/TaskContext";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useEffect } from "react";
 
 function TaskFormPage() {
-  const {register, handleSubmit} = useForm()
+  const {register, handleSubmit, setValue} = useForm()
+  const {createTask, getTask, updateTask} = useTasks()
   const navigate = useNavigate()
-  const {createTask} = useTasks()
+  const params = useParams()
 
+
+  useEffect( ()=> {
+    async function loadTask(){
+      if(params.id){
+        const task = await getTask(params.id)
+        console.log(task);
+        setValue('title', task.title)
+        setValue('description', task.description)
+      }
+    }
+    loadTask()
+  },[])
 
   const onSubmit = handleSubmit((data)=> {
-      createTask(data);
-      navigate('/tasks')
+      if(params.id ){
+        updateTask(params.id, data)
+      } else {
+        createTask(data)
+      }
+      navigate("/tasks")
   })
 
   return (
     <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md m-auto">
 
-      <form action="" onSubmit={onSubmit}>
-        <input type="text" placeholder="Title" {...register("title")} className="w-full bg-zinc-500 text-white px-4 py-2 rounded-md my-2" autoFocus/>
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Title" {...register("title")}  className="w-full bg-zinc-500 text-white px-4 py-2 rounded-md my-2" autoFocus/>
         <textarea rows="3" placeholder="Description" {...register("description")} className="w-full bg-zinc-500 text-white px-4 py-2 rounded-md my-2"></textarea>
         <button type="submit" className="w-16 bg-zinc-600 rounded-md">Save</button>
       </form>
